56bf2a17ff246d21b9e003b61bfa9e6e59bcd538
Joe <overtonje@gmail.com>
2020-12-11 12:05:51 -0600

    adding methods to the owner repo

--- DogGo/Repositories/DogRepository.cs
+++ DogGo/Repositories/DogRepository.cs
@@ -9,44 +9,44 @@ namespace DogGo.Repositories
 {
     public class DogRepository
     {
-        public SqlConnection Connection
-        {
-            get
-            {
-                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-            }
-        }
+        //public SqlConnection Connection
+        //{
+        //    get
+        //    {
+        //        return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        //    }
+        //}
 
-        public List<Dog> GetDogsByOwnerId(int id)
-        {
-            using (SqlConnection conn = Connection)
-            {
-                conn.Open();
-                using (SqlCommand cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = @"
-                        SELECT [Id], [Name], [OwnerId], [Breed], [Notes], [ImageUrl]
-                        FROM Dog
-                        WHERE [OwnerId] = @id
-                    ";
-                    cmd.Parameters.AddWithValue("@id", id);
+        //public List<Dog> GetDogsByOwnerId(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                SELECT [Id], [Name], [OwnerId], [Breed], [Notes], [ImageUrl]
+        //                FROM Dog
+        //                WHERE [OwnerId] = @id
+        //            ";
+        //            cmd.Parameters.AddWithValue("@id", id);
 
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.Read())
-                    {
-                        Dog dog = new Dog 
-                        {
-                        
-                        
-                        }
+        //            SqlDataReader reader = cmd.ExecuteReader();
+        //            if (reader.Read())
+        //            {
+        //                Dog dog = new Dog
+        //                {
 
 
-                    }
+        //                };
 
-                }
+
+        //            }
+
+        //        }
             
             
-            }
-                    }
+        //    }
+        //            }
     }
 }
--- DogGo/Repositories/OwnerRepository.cs
+++ DogGo/Repositories/OwnerRepository.cs
@@ -97,5 +97,69 @@ namespace DogGo.Repositories
                 }
             }
         }
+        public Owner GetOwnerByEmail(string email)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], Email, Address, Phone, NeighborhoodId
+                        FROM Owner
+                        WHERE Email = @email";
+
+                    cmd.Parameters.AddWithValue("@email", email);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Owner owner = new Owner()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+                        reader.Close();
+                        return owner;
+                    }
+                    reader.Close();
+                    return null;
+                }
+            }
+        }
+
+        public void AddOwner(Owner owner)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Owner ([Name], Email, Phone, Address, NeighborhoodId)
+                    OUTPUT INSERTED.ID
+                    VALUES (@name, @email, @phoneNumber, @address, @neighborhoodId);
+                ";
+
+                    cmd.Parameters.AddWithValue("@name", owner.Name);
+                    cmd.Parameters.AddWithValue("@email", owner.Email);
+                    cmd.Parameters.AddWithValue("@phoneNumber", owner.Phone);
+                    cmd.Parameters.AddWithValue("@address", owner.Address);
+                    cmd.Parameters.AddWithValue("@neighborhoodId", owner.NeighborhoodId);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    owner.Id = id;
+
+                }
+            }
+        }
+
+
     }
 }
+
