656dd545a8f6fc1ccec854d54057ed8b41ca041f
Joe <overtonje@gmail.com>
2021-01-01 09:11:55 -0600

    added controller base

---
+++ DogGo/Controllers/DogGoControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DogGo.Controllers
+{
+    public class DoggoControllerBase : Controller
+    {
+        protected int GetCurrentUserId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+
+        protected string GetCurrentUserRole()
+        {
+            return User.FindFirstValue(ClaimTypes.Role);
+        }
+    }
+}
